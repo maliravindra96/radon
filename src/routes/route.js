08576f3b827d9e28826e9362ce98a3a9669c6ade
maliravindra96 <maliravindra96@gmail.com>
2022-06-05 16:53:56 +0530

    assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,30 +1,24 @@
 const express = require('express');
-const externalModule = require('./logger')
+const externalModule1= require('../logger/logger');
+const externalModule2= require('../util/helper');
+const externalModule3= require('../validator/formatter');
+
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    console.log('The constant in logger route has a value '+externalModule.endpoint)
-    console.log('The current batch is '+externalModule.batch)
-    externalModule.log()
-    res.send('My first ever api!')
+  externalModule1.welcome()
+  externalModule2.printDate()
+  externalModule2.printMonth()
+  externalModule2.getBatchInfo()
+  externalModule3.trimIt()
+  externalModule3.changetoLowerCase()
+  externalModule3.changetoUpperCase()
+
+  
+    res.send("this is my first solution")
 });
 
-router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
-});
 
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
-});
 
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
-
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
-
-module.exports = router;
-// adding this comment for no reason
+module.exports= router;
