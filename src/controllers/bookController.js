b8763c2963938ece73dbacfbe20ece34aa086a9d
maliravindra96 <maliravindra96@gmail.com>
2022-06-11 18:34:58 +0530

    assignment/session/middleware-1

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -7,35 +7,21 @@ const createBook= async function (req, res) {
     let savedData= await BookModel.create(data)
     res.send({msg: savedData})
 }
-
-
-
-
-
-
 const getBooksData = async function (req, res) {
     let allBooks = await BookModel.find({ authorName: "HO" })
     console.log(allBooks)
     if (allBooks.length > 0) res.send({ msg: allBooks, condition: true })
     else res.send({ msg: "No books found", condition: false })
 }
-
-
 const updateBooks = async function (req, res) {
-    let data = req.body // {sales: "1200"}
-    // let allBooks= await BookModel.updateMany( 
-    //     { author: "SK"} , //condition
-    //     { $set: data } //update in data
-    //  )
+    let data = req.body 
     let allBooks = await BookModel.findOneAndUpdate(
         { authorName: "ABC" }, //condition
         { $set: data }, //update in data
         { new: true, upsert: true } ,// new: true - will give you back the updated document // Upsert: it finds and updates the document but if the doc is not found(i.e it does not exist) then it creates a new document i.e UPdate Or inSERT
     )
-
     res.send({ msg: allBooks })
 }
-
 const deleteBooks = async function (req, res) {
     // let data = req.body 
     let allBooks = await BookModel.updateMany(
@@ -43,12 +29,8 @@ const deleteBooks = async function (req, res) {
         { $set: { isDeleted: true } }, //update in data
         { new: true } ,
     )
-
     res.send({ msg: allBooks })
 }
-
-
-
 const totalSalesPerAuthor = async function (req, res) {
     // let data = req.body 
     let allAuthorSales = await BookModel.aggregate(
@@ -57,23 +39,17 @@ const totalSalesPerAuthor = async function (req, res) {
             { $sort: { totalNumberOfSales: -1 } }
         ]
     )
-
     res.send({ msg: allAuthorSales })
 }
-
-
-
-
-// CRUD OPERATIONS:
-// CREATE
-// READ
-// UPDATE
-// DELETE
-
-
-
 module.exports.createBook = createBook
 module.exports.getBooksData = getBooksData
 module.exports.updateBooks = updateBooks
 module.exports.deleteBooks = deleteBooks
 module.exports.totalSalesPerAuthor = totalSalesPerAuthor
+
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
+
+const route =require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+
 const app = express();
+const date = require("date-and-time");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://maliravindra96:EVqz2rukT8CYHqW7@cluster0.mkpwdi5.mongodb.net/mongo_book_pop_ref", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
+app.use(function(req, res, next) {
+    const value = date.format(new Date(),'YYYY-MM-DD HH:mm:ss');
+    console.log(value,',', req.ip,',', req.url)
+    next();
   }
   );
-
 app.use('/', route);
-
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
